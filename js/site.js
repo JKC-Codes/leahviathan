222b4637feba4ea2f6e308490aba142bbf99b78f
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2020-04-30 18:44:02 +0100

    Remove unnecessary dataset from toggle

--- js/site.js
+++ js/site.js
@@ -1,38 +1,4 @@
-var toggle = document.querySelector('#site-animations-toggle');
-var checkbox = toggle.querySelector('input');
-var storedPreference = localStorage.getItem('prefersReducedMotion');
-var mediaQueryList = window.matchMedia('(prefers-reduced-motion: reduce)');
-
-function updateToggle() {
-	checkbox.checked ? toggle.dataset.checked = 'true' : toggle.dataset.checked = 'false';
-}
-
-// Honour previously set preferences
-if (storedPreference != null) {
-	storedPreference == 'reduce' ? checkbox.checked = false : checkbox.checked = true;
-}
-// Honour system preferences
-else {
-	mediaQueryList.matches ? checkbox.checked = false : checkbox.checked = true;
-}
-
-// Initialise toggle
-toggle.removeAttribute('hidden');
-updateToggle();
-
-// Listen for switches
-toggle.addEventListener('change', function() {
-	function setPreference(preference) {
-		localStorage.setItem('prefersReducedMotion', preference);
-	}
-	checkbox.checked ? setPreference('no-preference') : setPreference('reduce');
-	updateToggle();
-})
-
-
-
-
-
+// NAV MENU
 var container = document.querySelector('#site-nav');
 var button = document.querySelector('#site-nav-button');
 var text = document.querySelector('#site-nav-button-text');
@@ -54,3 +20,33 @@ button.addEventListener('click', function(event) {
 		text.textContent = 'Close menu';
 	}
 });
+
+
+
+
+
+// ANIMATIONS TOGGLE
+var toggle = document.querySelector('#site-animations-toggle');
+var checkbox = toggle.querySelector('input');
+var storedPreference = localStorage.getItem('prefersReducedMotion');
+var prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+
+// Honour previously set preferences
+if (storedPreference != null) {
+	storedPreference == 'reduce' ? checkbox.checked = false : checkbox.checked = true;
+}
+// Honour system preferences if no stored preference
+else {
+	prefersReducedMotion ? checkbox.checked = false : checkbox.checked = true;
+}
+
+// Listen for switches
+toggle.addEventListener('change', function() {
+	function setPreference(preference) {
+		localStorage.setItem('prefersReducedMotion', preference);
+	}
+	checkbox.checked ? setPreference('no-preference') : setPreference('reduce');
+})
+
+// Show toggle
+toggle.removeAttribute('hidden');
